490f12ee3e2002b267f021fc4795b803731829dc
hema <hemangibadgujar83@gmail.com>
2022-03-12 00:12:53 +0530

    fourth page

--- product.py
+++ product.py
@@ -5,7 +5,7 @@ from turtle import title
 from PIL import Image,ImageTk
 from tkinter import ttk,messagebox
 import sqlite3
-
+import os
 from pkg_resources import EntryPoint
 
 class productClass:
@@ -69,10 +69,11 @@ class productClass:
         cmb_status.current(0)
 
         #=======================Buttons=================
-        btn_add=Button(product_Frame,text="Save",command=self.add,font=("goudy old style",15),bg="blue",fg="white",cursor="hand2").place(x=10,y=400,width=100,height=40)
-        btn_update=Button(product_Frame,text="Update",command=self.update,font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=120,y=400,width=100,height=40)
-        btn_delete=Button(product_Frame,text="Delete",command=self.delete,font=("goudy old style",15),bg="#f44336",fg="white",cursor="hand2").place(x=230,y=400,width=100,height=40)
-        btn_clear=Button(product_Frame,text="Clear",command=self.clear,font=("goudy old style",15),bg="#607d8b",fg="white",cursor="hand2").place(x=340,y=400,width=100,height=40)
+        btn_add=Button(product_Frame,text="Save",command=self.add,font=("goudy old style",15),bg="blue",fg="white",cursor="hand2").place(x=10,y=400,width=80,height=28)
+        btn_update=Button(product_Frame,text="Update",command=self.update,font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=95,y=400,width=80,height=28)
+        btn_delete=Button(product_Frame,text="Delete",command=self.delete,font=("goudy old style",15),bg="#f44336",fg="white",cursor="hand2").place(x=180,y=400,width=80,height=28)
+        btn_clear=Button(product_Frame,text="Clear",command=self.clear,font=("goudy old style",15),bg="#607d8b",fg="white",cursor="hand2").place(x=265,y=400,width=80,height=28)
+        btn_back=Button(product_Frame,text="Back",command=self.back,font=("goudy old style",15),bg="#009688",fg="white",cursor="hand2").place(x=350,y=400,width=80,height=28)
 
         #======SearchFrame=====
         SearchFrame=LabelFrame(self.root,text="Search Employee",font=("goudy old style",12,"bold"),bd=2,relief=RIDGE,bg="white")
@@ -298,6 +299,10 @@ class productClass:
                     messagebox.showerror("Error","No record found",parent=self.root)
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to :{str(ex)}",parent=self.root)
+
+    def back(self):
+        self.root.destroy()
+        os.system("python category.py")
 
 if __name__=="__main__":
  root=Tk()
