ee1cae4e1ed0d629710bd0838699b9cb3f316ed7
hema <hemangibadgujar83@gmail.com>
2022-03-13 10:00:18 +0530

    first login page

--- login.py
+++ login.py
@@ -167,7 +167,6 @@ class Login_system:
         else:
             messagebox.showerror("Error","Invalid OTP,Try Again",parent=self.forget_win)
 
-
     def send_email(self,to_):
         s=smtplib.SMTP('smtp.gmail.com',587)
         s.starttls()
